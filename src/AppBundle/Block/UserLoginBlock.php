b0ac3d49df4ad06a48bf4637bad72eb318a61ef8
Your Name <you@example.com>
2015-01-17 12:00:36 +0300

    user login block added

---
+++ src/AppBundle/Block/UserLoginBlock.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Block;
+
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Sonata\UserBundle\Model\UserInterface;
+
+
+class UserLoginBlock extends ContainerAwareBlock
+{
+
+
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $isLoggedIn = $user instanceof UserInterface;
+
+        $csrfToken = $this->getContainer()->get('form.csrf_provider')->generateCsrfToken('authenticate');
+
+        return $this->renderResponse('AppBundle:Default\\Block:user_login.html.twig', array(
+            'block'     => $blockContext->getBlock(),
+            //'error'         => '',
+            'csrf_token' => $csrfToken,
+            'isLoggedIn' => $isLoggedIn,
+        ), $response);
+
+    }
+}
