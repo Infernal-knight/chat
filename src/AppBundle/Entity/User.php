51351d7f6d2d331705dfd5404d1adec065402ad9
root <root@princessluna.tld>
2015-01-20 14:58:36 +0400

    user entity hotfix

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -104,6 +104,7 @@ class User extends BaseUser
      */
     public function __construct()
     {
+        parent::__construct();
         $this->messages = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
